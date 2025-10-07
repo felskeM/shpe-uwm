201e49f3c5129f2d684830bbd30fad1740354705
Matthew Felske <43674484+felskeM@users.noreply.github.com>
2025-10-07 02:59:34 +0000

    added IP for dev origins settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 import bundleAnalyzer from "@next/bundle-analyzer";
 
+/** @type {import('next').NextConfig} */
+
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
 });
@@ -8,6 +10,11 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  allowedDevOrigins: [
+    '127.0.0.1',
+    'localhost',
+    '10.0.14.252'
+  ],
 };
 
 export default withBundleAnalyzer(nextConfig);
