c917f29f8c78890099447ec2c2316b59c263e3dc
Matthew Felske <43674484+felskeM@users.noreply.github.com>
2025-10-08 02:51:11 +0000

    Changed API files and folders for github pages to run static

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,16 @@
 /** @type {import('next').NextConfig} */
 const isStatic = process.env.NEXT_STATIC_EXPORT === '1';
-const repo = 'shpe-uwm'; // (for https://felskeM.github.io/shpe-uwm)
+const repo = 'shpe-uwm';
 
 export default {
-  // Only enable static export for GH Pages builds:
   output: isStatic ? 'export' : undefined,
-
-  // GH Pages serves from /<repo>, so set a basePath & assetPrefix in static mode:
   basePath: isStatic ? `/${repo}` : '',
   assetPrefix: isStatic ? `/${repo}` : '',
-
-  // next/image must be unoptimized in export mode (no server to optimize)
   images: { unoptimized: isStatic },
-
-  // Helps GH Pages find folder-based index.html files
   trailingSlash: isStatic,
-
+  typedRoutes: true,
+  eslint: { ignoreDuringBuilds: true },
   experimental: {
-    typedRoutes: true,
+    // remove experimental.typedRoutes;
   },
 };
